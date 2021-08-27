69035bfacdcf54a750663c621ce9e659bfa9c4d5
Denys <dentor2005@gmail.com>
2021-08-27 14:13:14 +0200

    UserSharedPreferences + Composition local provider

--- app/src/main/java/com/example/streetmusic2/ui/theme/Theme.kt
+++ app/src/main/java/com/example/streetmusic2/ui/theme/Theme.kt
@@ -4,44 +4,63 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
+import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Purple,
+    primaryVariant = Purple,
+
+    secondary = Sky,
+    secondaryVariant = Sky,
+
+    background = White,
+    surface = White,
+    error = DarkRed,
+
+    onPrimary = White,
+    onSecondary = DarkGrey,
+
+    onBackground = DarkGrey,
+    onSurface = DarkGrey,
+    onError = White
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Purple,
+    primaryVariant = Purple,
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    secondary = Sky,
+    secondaryVariant = Sky,
+
+    background = White,
+    surface = White,
+    error = DarkRed,
+
+    onPrimary = White,
+    onSecondary = DarkGrey,
+
+    onBackground = DarkGrey,
+    onSurface = DarkGrey,
+    onError = White
+
 )
 
 @Composable
 fun StreetMusic2Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = if (darkTheme) DarkColorPalette else LightColorPalette,
         typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+        shapes = Shapes
+    ) {
+        CompositionLocalProvider(
+            LocalRippleTheme provides MyRippleTheme,
+            content = content
+        )
+    }
 }
+
